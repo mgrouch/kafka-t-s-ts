edcda4e7965b6775dd5688fd2be86a6e3e6144c0
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-23 22:02:06 -0400

    initial

--- tx-cache-app/src/main/java/com/example/txcache/app/TxCacheApplication.java
+++ tx-cache-app/src/main/java/com/example/txcache/app/TxCacheApplication.java
@@ -1,13 +1,19 @@
 package com.example.txcache.app;
 
+import org.apache.kafka.streams.KafkaStreams;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public final class TxCacheApplication {
     public static void main(String[] args) throws Exception {
-        var ctx = new SpringApplicationBuilder().sources(Empty.class).web(WebApplicationType.NONE).initializers(new TxCacheInitializer()).run(args);
-        ctx.getBean(StreamsApp.class).buildAndStart();
-        Thread.currentThread().join();
+        try (var ctx = new SpringApplicationBuilder()
+                .sources(Empty.class)
+                .web(WebApplicationType.NONE)
+                .initializers(new TxCacheInitializer())
+                .run(args);
+            var streams = ctx.getBean(StreamsApp.class).buildAndStart()) {
+            Thread.currentThread().join();
+        }
     }
 
     public static final class Empty {
